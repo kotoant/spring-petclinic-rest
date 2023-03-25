b1d69b8f7422b516794a23322daf3827ca24a913
Anton Kotov <kotov-anton@yandex.ru>
2023-03-25 12:08:38 +0300

    UPD: docker-build.yml: added docker build and push workflow

--- run-java.sh
+++ run-java.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-: "${SERVER:=tomcat}"
-
-if [[ "$SERVER" =~ ^(jetty|netty|tomcat|undertow)$ ]]; then
+if [[ "${SERVER:=tomcat}" =~ ^(jetty|netty|tomcat|undertow)$ ]]; then
   RUN_JAVA_OPTIONS="${JAVA_OPTIONS:-} -Dserver.port=${PORT:-9966} -Dspring.profiles.active=${PROFILE:=webmvc-jdbc} -XX:+PrintFlagsFinal --enable-preview -XX:StartFlightRecording:filename=/usr/local/app/dump/$SERVER-$PROFILE-recording.jfr,dumponexit=true -XX:+ExitOnOutOfMemoryError"
 
   echo exec java $RUN_JAVA_OPTIONS -jar $SERVER.jar "$@"
