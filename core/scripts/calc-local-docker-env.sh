80d813fdc7adb9be6e241bf4661339cd1de5cbfc
Anton Kotov <kotov-anton@yandex.ru>
2023-03-25 17:30:17 +0300

    UPD: calc-local-docker-env.sh: renamed local env file;

--- core/scripts/calc-local-docker-env.sh
+++ core/scripts/calc-local-docker-env.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-echo -n 'DB_HOST=localhost:' >.env
+echo -n 'DB_HOST=localhost:' >local.env
 # keep in sync with org.springframework.samples.petclinic.test.BaseTestcontainersTest.POSTGRESQL_CONTAINER and core/scripts/stop-and-remove-testcontainers.sh:
-echo $(docker ps -a -q -f "label=org.springframework.samples.petclinic.testcontainers.postgres=petclinic" | xargs -I{} docker port {} 5432 | cut -c9-) >>.env
+echo $(docker ps -a -q -f "label=org.springframework.samples.petclinic.testcontainers.postgres=petclinic" | xargs -I{} docker port {} 5432 | cut -c9-) >>local.env
