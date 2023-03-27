6cbf85492c97c4c7827318156c7bf2e38626423e
Anton Kotov <kotov-anton@yandex.ru>
2023-03-27 17:42:21 +0300

    added spring cloud configuration; updated config;

--- core/scripts/calc-local-docker-env.sh
+++ core/scripts/calc-local-docker-env.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-echo -n 'DB_HOST=localhost:' >local.env
 # keep in sync with org.springframework.samples.petclinic.test.BaseTestcontainersTest.POSTGRESQL_CONTAINER and core/scripts/stop-and-remove-testcontainers.sh:
-echo $(docker ps -a -q -f "label=org.springframework.samples.petclinic.testcontainers.postgres=petclinic" | xargs -I{} docker port {} 5432 | cut -c9-) >>local.env
+DB_PORT=$(docker ps -a -q -f "label=org.springframework.samples.petclinic.testcontainers.postgres=petclinic" | xargs -I{} docker port {} 5432 | cut -c9-)
+
+echo -n 'DB_HOST=localhost:' >local.env
+echo "$DB_PORT" >>local.env
+
+echo -n 'DB_HOST=docker.for.mac.host.internal:' >docker.env
+echo "$DB_PORT" >>docker.env
